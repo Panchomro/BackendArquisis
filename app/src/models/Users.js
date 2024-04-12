f48c441bbe6630eabb227f8a71ef93884810479e
LukasGuthrie <lguthrie@uc.cl>
2024-04-12 00:23:57 -0400

    feat(user): create user model

---
+++ app/src/models/Users.js
@@ -0,0 +1,36 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('postgres://panchomro:Arya1234@db:5432/e0arquisis');
+
+
+const User = sequelize.define('User', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+}, {
+    timestamps: false,
+    tableName: 'InfoCompras'
+});
+
+module.exports = InfoCompras;
