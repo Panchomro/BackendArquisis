fc23ead01879b839cbd356ede7abbc5ea486c23a
LukasGuthrie <lguthrie@uc.cl>
2024-04-15 11:30:05 -0400

    feat(user): add unique attribute to mail

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -11,6 +11,7 @@ const User = sequelize.define('User', {
     email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true
     },
     password: {
         type: DataTypes.STRING,
