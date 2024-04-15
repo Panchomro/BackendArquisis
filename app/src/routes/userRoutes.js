53e5894e12a2a88c2a326c32d88fed4c02e4b907
LukasGuthrie <lguthrie@uc.cl>
2024-04-15 17:04:20 -0400

    feat(routes): add routes get, post, update mathods for users

---
+++ app/src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const UserController = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/user/new', UserController.createUser);
+
+router.get('/user/:id', UserController.getUser);
+
+router.update('/user/:id/update', UserController.updateUser);
+
+module.exports = router;
