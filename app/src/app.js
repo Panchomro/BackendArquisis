7c4371500f225c3cd925b586eb5676c86c12fe9b
LukasGuthrie <lguthrie@uc.cl>
2024-04-16 22:58:06 -0400

    feat(user): add user model and router to middleware

--- app/src/app.js
+++ app/src/app.js
@@ -4,7 +4,11 @@ const express = require('express');
 
 const flightRoutes = require('./routes/flightRoutes');
 
+const userRoutes = require('./routes/userRoutes');
+
 const Flight = require('./models/Flight');
+
+const User = require('./models/User');
 
 const app = express();
 
@@ -13,12 +17,16 @@ const PORT = process.env.PORT;
 // Middleware
 app.use(express.json());
 app.use('/', flightRoutes);
+
+app.use('/', userRoutes);
 app.use(express.urlencoded({ extended: false }));
 
 
 async function syncDatabase() {
   try {
     await Flight.sync({ force: false });
+
+    await User.sync({ force: false });
     console.log('Modelo sincronizado con la base de datos');
   } catch (error) {
     console.error('Error al sincronizar el modelo con la base de datos:', error);
