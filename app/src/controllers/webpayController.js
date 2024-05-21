e8405d639e0da6af94ba9584572a809f0da00ad0
SEBASTIAN RASMUSSEN <srasmussenc@uc.cl>
2024-05-21 00:43:04 -0400

    avance espero

---
+++ app/src/controllers/webpayController.js
@@ -0,0 +1,56 @@
+const { WebpayPlus } = require('transbank-sdk');
+const { Options, IntegrationApiKeys, Environment, IntegrationCommerceCodes } = require('transbank-sdk');
+const Transaction = require('transbank-sdk/dist/es5/transbank/webpay/webpay_plus/transaction');
+const Flight = require('../models/Flight'); // Importa el modelo de vuelo
+const uuid = require('uuid-random');
+
+class WebpayController {
+  static async createTransaction(req, res) {
+    try {
+      console.log('Authorization Header:', req.headers.authorization);
+      const { quantity, ip, flightId } = req.body;
+      const userId = req.auth.sub;
+      console.log('idVuelo:', flightId);
+      console.log('user_id:', userId);
+
+    
+      const vuelo = await Flight.findByPk(flightId);
+      if (!vuelo) {
+        throw new Error('Vuelo no encontrado');
+      }
+
+      const amount = vuelo.price * quantity;
+
+      const buyOrder = uuid();
+
+      const tx = new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration));
+      const trx = await tx.create(buyOrder, ip, amount, `http://localhost:3000/confirm-transaction/${flightId}/${userId}/${quantity}/${ip}`);
+
+      res.status(200).json({ url: trx.url, token: trx.token });
+    } catch (error) {
+      console.error('Error creating transaction:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  }
+
+  static async confirmTransaction(req, res) {
+    try {
+      // 1. Obtén el token de la transacción del cuerpo de la solicitud
+      const { token } = req.body;
+
+      // 2. Confirma la transacción de WebPay
+      const tx = new WebpayPlus.Transaction(new Options(IntegrationCommerceCodes.WEBPAY_PLUS, IntegrationApiKeys.WEBPAY, Environment.Integration));
+      const confirmedTx = await tx.commit(token);
+
+      // 3. Actualiza el estado de la compra en tu base de datos
+
+      // 4. Envía una respuesta exitosa
+      res.status(200).json({ message: 'Transacción confirmada exitosamente' });
+    } catch (error) {
+      console.error('Error confirming transaction:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  }
+}
+
+module.exports = WebpayController;
--- app/src/routes/infoComprasRoutes.js
+++ app/src/routes/infoComprasRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const InfoComprasController = require('../controllers/InfoComprasController');
+const WebpayController = require('../controllers/webpayController');
 const checkJwt = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/buy', checkJwt, InfoComprasController.createInfoCompras);
+
+router.post('/create-transaction', checkJwt, WebpayController.createTransaction);
 
 router.post('/flights/validations/:request_id', InfoComprasController.manejarValidation);
 
