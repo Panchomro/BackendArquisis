ae96bf579782ab5359af85bdaef2628d33565fa5
Panchomro <panchomr.2000@uc.cl>
2024-05-22 19:19:46 -0400

    fix: CodeDeploy

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,2 +1,7 @@
 echo "Starting application..."
-docker-compose --file /home/ubuntu/docker-compose.production.yml up -d
+
+docker-compose --file /home/ubuntu/docker-compose.production.yml up -d db
+docker-compose --file /home/ubuntu/docker-compose.production.yml up -d app
+sleep 1
+
+docker-compose --file /home/ubuntu/docker-compose.production.yml up -d mqtt
