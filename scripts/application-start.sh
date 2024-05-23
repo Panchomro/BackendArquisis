3b219862417d1366e0c564f4c26c067afb9a8e65
Panchomro <panchomr.2000@uc.cl>
2024-05-23 05:41:25 -0400

    feat: MergedCD

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -2,14 +2,14 @@ echo "Starting application..."
 
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d db
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d app
-sleep 3
+sleep 8
 
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d mqtt
-sleep 3
+sleep 8
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d redis
-sleep 3
+sleep 8
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d producer
-sleep 3
+sleep 8
 docker-compose --file /home/ubuntu/docker-compose.production.yml up -d consumer
-sleep 3
+sleep 8
 
